bb3d536eafbb63c574f4af7b60d3d960bfdd3f19
liwei <liwei@uama.com.cn>
2018-04-01 20:15:21 +0800

    增加 Retrofit 和 RxJava2 的依赖

--- app/src/main/java/com/niniliwei/kgank/MainActivity.kt
+++ app/src/main/java/com/niniliwei/kgank/MainActivity.kt
@@ -2,11 +2,22 @@ package com.niniliwei.kgank
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recycler_view.layoutManager = LinearLayoutManager(this)
+
+    }
+
+    private fun refresh() {
+        if (!refresh_layout.isRefreshing) refresh_layout.isRefreshing = true
+
+
     }
 }
